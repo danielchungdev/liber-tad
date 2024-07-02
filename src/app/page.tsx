c8da77d49a437af9b4b1aebb3086dc72dd004198
danielchungdev <pikachungg@gmail.com>
2024-07-02 16:46:30 -0400

    finished landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { Logo } from "@/components/Logo";
 import { Navbar } from "@/components/Navbar";
 import { SearchBar } from "@/components/SearchBar";
@@ -5,13 +6,13 @@ import { SearchBar } from "@/components/SearchBar";
 export default function Home() {
   return (
     <main>
-      {/* {Pushing for development branch} */}
-      <p className="text-header font-libre">Hello world</p>
-      <p className="text-paragraph font-source">Hello world</p>
-      <p className="text-small-body">Hello world</p>
-      <Navbar/>
-      <Logo/>
-      <SearchBar/>
+      <Navbar className="m-4"/>
+      <div className="grid place-content-center h-[70vh]">
+        <Logo className="text-center"/>
+        <SearchBar className="my-4"/>
+        <p className="text-center">Not sure what to read? <span className="text-primary hover:text-accent transition duration-300 ease-in-out">Click here to browse</span></p>
+      </div>
+      <Footer/>
     </main>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+    return(
+        <div className="bg-accent fixed inset-x-0 bottom-0">
+            <p className="text-center text-white py-2 text-small-body">Developed by Daniel Chung ❤️</p>
+        </div>
+    )
+}
