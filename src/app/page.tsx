72c42e682267798589a8b9e45b91d6174d046944
danielchungdev <pikachungg@gmail.com>
2024-07-02 23:28:19 -0400

    small fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Footer } from "@/components/Footer";
 import { Logo } from "@/components/Logo";
 import { Navbar } from "@/components/Navbar";
 import { SearchBar } from "@/components/SearchBar";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       <div className="grid place-content-center h-[70vh]">
         <Logo className="text-center"/>
         <SearchBar className="my-4"/>
-        <p className="text-center">Not sure what to read? <span className="text-primary hover:text-accent transition duration-300 ease-in-out">Click here to browse</span></p>
+        <p className="text-center">Not sure what to read? <Link href={"/books"} className="text-primary hover:text-accent transition duration-700 ease-in-out">Click here to browse</Link></p>
       </div>
       <Footer/>
     </main>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link"
 
 export const Footer = () => {
     return(
         <div className="bg-accent fixed inset-x-0 bottom-0">
-            <p className="text-center text-white py-2 text-small-body">Developed by Daniel Chung ❤️</p>
+            <p className="text-center text-white py-2 text-small-body">Want to see more? Check out <Link className="hover:text-text transition duration-700 ease-in-out" href="https://www.danielchung.dev" target="_blank">www.danielchung.dev</Link></p>
         </div>
     )
 }
