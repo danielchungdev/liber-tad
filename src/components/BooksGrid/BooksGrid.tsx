73e5bdd3e12ea3ab5bc37cbcd6e91870005b2a4e
danielchungdev <pikachungg@gmail.com>
2024-07-04 06:32:38 -0400

    finished detail page

--- src/components/BooksGrid/BooksGrid.tsx
+++ src/components/BooksGrid/BooksGrid.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react"
 import { Book } from "@/types/Book"
+import Link from "next/link"
 
 interface BooksGridProps {
     data: Book[] | undefined
@@ -13,10 +14,10 @@ export const BooksGrid: FC<BooksGridProps> = ({ data }) => {
                     <div className="grid grid-cols-8 gap-4">
                         {
                             data.map( (book) => (
-                                <div key={book.id} className="text-center my-1 font-libre text-paragraph grid bg-secondary shadow-md p-2 rounded-lg">
+                                <Link href={`/book/${book.id}`} key={book.id} className="text-center my-1 font-libre text-paragraph grid bg-secondary shadow-md p-2 rounded-lg">
                                     <img src={book.formats["image/jpeg"]} className="h-[200px] m-auto" alt={`${book.title}'s cover image`}/>
                                     <p>{book.title}</p>
-                                </div>
+                                </Link>
                             ))
                         }
                     </div>
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react"
-
+import Link from "next/link"
 interface LogoProps {
     size?: "sm" | "lg",
     className?: string,
@@ -17,6 +17,6 @@ export const Logo: FC<LogoProps> = ({ size = "lg", className }) => {
     }
 
     return (
-        <p className={`font-libre italic ${determineSize(size)} ${className}`}><span className="text-primary ">Liber</span>-tad</p>
+        <Link href="/" className={`font-libre italic ${determineSize(size)} ${className}`}><span className="text-primary ">Liber</span>-tad</Link>
     )
 }
