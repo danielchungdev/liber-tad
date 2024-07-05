3c63d8252558d341d8f3bcda0c6e705018ce8848
danielchungdev <pikachungg@gmail.com>
2024-07-05 01:02:06 -0400

    last page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,30 +16,36 @@ export default function AboutHome() {
                     <section>
                         <h1 className="text-header font-libre my-4">User Flow</h1>
                         <p className="mb-2">This is the userflow created with Figma, representing the user's journey throughout the application.</p>
-                        <Image className="m-auto" src="/images/userflow.png" height={200} width={700} alt={"User flow diagram of the app"}/>
+                        <Image className="m-auto" src="/images/userflow.png" height={200} width={700} alt={"User flow diagram of the app"} />
                     </section>
                     <section>
                         <h1 className="text-header font-libre my-4">Design System</h1>
                         <p className="mb-2">Here's the design system to follow throughout the application, I ended up changing the way tags look as it had poor readability.</p>
-                        <Image className="m-auto" src="/images/Design-system.png" height={200} width={700} alt={"Design system for the app"}/>
+                        <Image className="m-auto" src="/images/Design-system.png" height={200} width={700} alt={"Design system for the app"} />
                     </section>
                     <section>
                         <h1 className="text-header font-libre my-4">Wireframes</h1>
                         <p className="mb-2">Here are the wireframes for the application. I Ended up ditching the mobile implementation as for time constraints.</p>
-                        <Image className="m-auto" src="/images/wireframes.png" height={200} width={700} alt={"Wireframes for the app"}/>
+                        <Image className="m-auto" src="/images/wireframes.png" height={200} width={700} alt={"Wireframes for the app"} />
                     </section>
                     <section>
                         <h1 className="text-header font-libre my-4">Final Design</h1>
-                        <Image className="m-auto" src="/images/Designs.png" height={200} width={700} alt={"Final design for the app"}/>
+                        <Image className="m-auto" src="/images/Designs.png" height={200} width={700} alt={"Final design for the app"} />
                     </section>
                     <section>
                         <h1 className="text-header font-libre my-4">Potential Application (Failed implementation)</h1>
                         <p className="mb-2">This is the original idea I tried implementing. It uses this new web design element that makes it very interactive. The idea was to have all the titles to the right as a list, and as you scrolled down it would go down the titles and show the book to the right. Due to the lack of features and not sure how to implement them I decided to go the simpler route, but this idea is archieved and I will be working on it as a personal project.</p>
-                        <Image className="m-auto" src="/gif/original-idea.gif" height={200} width={700} alt={"Original app idea"}/>
+                        <Image className="m-auto" src="/gif/original-idea.gif" height={200} width={700} alt={"Original app idea"} />
+                    </section>
+                    <section>
+                        <h1 className="text-header font-libre my-4">Branching Strategy</h1>
+                        <p className="mb-2">For branching strategy I went with the standard master, development and feature branches. Only commiting to master after a completing a feature(pages)</p>
+
+                        <Image className="m-auto" src="/images/branches.png" height={200} width={700} alt={"Final design for the app"} />
                     </section>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </main>
     );
 }
